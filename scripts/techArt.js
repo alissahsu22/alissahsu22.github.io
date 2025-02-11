45754d2024079ffa96c175f4f972951d168b0546
Alissa Hsu <alissaohsu@yahoo.com>
2025-02-11 14:29:56 +0100

    Update techArt.js

--- scripts/techArt.js
+++ scripts/techArt.js
@@ -16,18 +16,14 @@ playBtn.addEventListener('click',(event)=>{
 })
 
 const projects = {
-    'Emotion-Based Music Recommender System Using Image Classification':'../projects/emotionDetection/templates/index.html',
-    'Computer Vision Golf Ball Tracking and Detection Model': 'javascript:openVideo()',
     'Generative Art Gallery': '../projects/drawing',
     'Web Development Projects': '../projects/webdev',
     'Full Stack E-Commerce Site with Shopify':'https://aronicadiffuser.myshopify.com/',
-    
+    'In The Mind Palace (Animation)': 'https://www.youtube.com/watch?v=kPwkQ5NfvDQ',
+    '2D City (AR)':'https://youtube.com/shorts/Pgdrf1bA41g',
 };
 
 
-function openVideo() {
-    window.open('projects/golfball.avi', '_blank', 'width=800,height=600');
-}
 
 document.addEventListener('DOMContentLoaded', () => {
     const starWrapper = document.querySelector('.starWrapper');
