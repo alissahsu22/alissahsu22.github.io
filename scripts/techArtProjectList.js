a74f86eb961114f206233d85ecb5241e67ac0f36
Alissa Hsu <alissaohsu@yahoo.com>
2025-06-23 10:55:13 -0400

    fixed techArt project list page

--- scripts/techArtProjectList.js
+++ scripts/techArtProjectList.js
@@ -36,17 +36,6 @@ const projects = {
     },
 };
 
-// const experience = {
-//     'Software Engineer Intern @ Kaizen8': {
-//         description: 'At Kaizen8 as a Software Engineer Intern, I played a foundational role in launching a full-stack website to showcase the company’s newest product. I managed the project from concept to completion, aligning the site with the company’s goals and image. By integrating third-party APIs and Shopify tools, I enhanced user experience and site functionality. I also leveraged custom Shopify Liquid code to save over $500 by replacing costly app add-ons. Additionally, I integrated the site with in-house systems, streamlining exportation and reducing third-party fulfillment costs by 20%, ultimately improving operational efficiency and boosting sales performance..'
-//     },
-//     'Software Engineer Intern, Machine Learning @ Ideas Lab': {
-//         description: 'At Ideas lab as a Software Engineer intern with a focus on Machine Learning, I developed a computer vision model using YOLOv8 from scratch, designed to accurately track real golf balls based on a dataset of over 2,000 hand-drawn annotations with an accuracy of over 85%. To enhance the model’s detection robustness, engineered a synthetic data generator using OpenCV to produce more than 1,000 diverse images, simulating various trajectories and backgrounds. I also constructed convolutional neural networks in PyTorch and Tensorflow, focusing on refining each layer’s parameters to achieve optimal accuracy and efficiency to create the most accurate model possible. This work was integral to supporting in-app golf ball trajectory analysis.'
-//     },
-//     'Product Management Intern @ Kaizen8': {
-//         description: 'At Kaizen8 as a Product Management Intern, I contributed to the company’s growth, helping it become a top 100 Amazon seller globally. By conducting in-depth research on over 100 emerging products and broadening the company’s distribution network, the company was able to expand its product catalog to over 2,500 items. Additionally, I optimized the product preparation workflow, improving order fulfillment efficiency by 20% and streamlining export processes for faster, more efficient distribution.'
-//     }
-// }
 
 
 window.onload = () => {
