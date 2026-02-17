b1928b4413e6319199b51b29e4e0bf76e9e1fb75
alissahsu22 <aoh2024@nyu.edu>
2026-02-17 15:29:05 -0500

    added collapsable button for panel

--- projects/techArtProjects/memory-orbs/src/ControlsPanel.jsx
+++ projects/techArtProjects/memory-orbs/src/ControlsPanel.jsx
@@ -5,92 +5,92 @@ export default function ControlsPanel({
   fogEnabled, setFogEnabled,
   fogDensity, setFogDensity,
   floatiness, setFloatiness,
-  glowStrength, setGlowStrength
+  glowStrength, setGlowStrength,
+  panelOpen,
+  setPanelOpen
 }) {
   return (
-    <div className="panel">
-      <div className="panelHeader">
-        <div className="title">Scene Controls</div>
-      </div>
+    <div className={`panel ${panelOpen ? 'open' : 'closed'}`}>
+      <button
+        className="panelToggle"
+        onClick={() => setPanelOpen(!panelOpen)}
+        type="button"
+      >
+        {panelOpen ? 'Hide' : 'Scene Controls'}
+      </button>
 
-      <div className="row">
-        <div className="rowTop">
-          <span>Orbit Speed</span>
-          <span className="value">{orbitSpeed.toFixed(1)}</span>
-        </div>
-        <input
-          className="slider"
-          type="range"
-          min="0"
-          max="10"
-          step="0.1"
-          value={orbitSpeed}
-          onChange={(e) => setOrbitSpeed(Number(e.target.value))}
-        />
-      </div>
+      {panelOpen && (
+        <div className="panelBody">
+          <div className="panelHeader">
+            <div className="title">Scene Controls</div>
+          </div>
 
-      {/* <div className="row switchRow">
-        <div className="rowTop">
-          <span>Fog</span>
-          <label className="switch">
+          <div className="row">
+            <div className="rowTop">
+              <span>Orbit Speed</span>
+              <span className="value">{orbitSpeed.toFixed(1)}</span>
+            </div>
             <input
-              type="checkbox"
-              checked={fogEnabled}
-              onChange={(e) => setFogEnabled(e.target.checked)}
+              className="slider"
+              type="range"
+              min="0"
+              max="10"
+              step="0.1"
+              value={orbitSpeed}
+              onChange={(e) => setOrbitSpeed(Number(e.target.value))}
             />
-            <span className="sliderKnob" />
-          </label>
-        </div>
-      </div> */}
+          </div>
 
-      <div className={`row ${!fogEnabled ? 'disabled' : ''}`}>
-        <div className="rowTop">
-          <span>Fog</span>
-          <span className="value">{fogDensity.toFixed(3)}</span>
-        </div>
-        <input
-          className="slider"
-          type="range"
-          min="0"
-          max="0.12"
-          step="0.002"
-          value={fogDensity}
-          onChange={(e) => setFogDensity(Number(e.target.value))}
-          disabled={!fogEnabled}
-        />
-      </div>
+          <div className={`row ${!fogEnabled ? 'disabled' : ''}`}>
+            <div className="rowTop">
+              <span>Fog</span>
+              <span className="value">{fogDensity.toFixed(3)}</span>
+            </div>
+            <input
+              className="slider"
+              type="range"
+              min="0"
+              max="0.12"
+              step="0.002"
+              value={fogDensity}
+              onChange={(e) => setFogDensity(Number(e.target.value))}
+              disabled={!fogEnabled}
+            />
+          </div>
 
-      <div className="row">
-        <div className="rowTop">
-          <span>Floatiness</span>
-          <span className="value">{floatiness.toFixed(1)}</span>
-        </div>
-        <input
-          className="slider"
-          type="range"
-          min="0"
-          max="3"
-          step="0.1"
-          value={floatiness}
-          onChange={(e) => setFloatiness(Number(e.target.value))}
-        />
-      </div>
+          <div className="row">
+            <div className="rowTop">
+              <span>Floatiness</span>
+              <span className="value">{floatiness.toFixed(1)}</span>
+            </div>
+            <input
+              className="slider"
+              type="range"
+              min="0"
+              max="3"
+              step="0.1"
+              value={floatiness}
+              onChange={(e) => setFloatiness(Number(e.target.value))}
+            />
+          </div>
 
-      <div className="row">
-        <div className="rowTop">
-          <span>Glow</span>
-          <span className="value">{glowStrength.toFixed(1)}</span>
+          <div className="row">
+            <div className="rowTop">
+              <span>Glow</span>
+              <span className="value">{glowStrength.toFixed(1)}</span>
+            </div>
+            <input
+              className="slider"
+              type="range"
+              min="0"
+              max="6"
+              step="0.1"
+              value={glowStrength}
+              onChange={(e) => setGlowStrength(Number(e.target.value))}
+            />
+          </div>
         </div>
-        <input
-          className="slider"
-          type="range"
-          min="0"
-          max="3"
-          step="0.1"
-          value={glowStrength}
-          onChange={(e) => setGlowStrength(Number(e.target.value))}
-        />
-      </div>
+      )}
     </div>
   )
 }
